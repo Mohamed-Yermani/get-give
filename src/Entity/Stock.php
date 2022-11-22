71bc5120738a4cf4091bb02d864927dd58704d1a
mohamed yermani <mohamed.yermani@esprit.tn>
2022-11-22 01:48:25 +0100

    get&give

---
+++ src/Entity/Stock.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StockRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StockRepository::class)]
+class Stock
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $emplacement = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $reference = null;
+
+    #[ORM\Column]
+    private ?int $nbreProdEnstock = null;
+
+    #[ORM\ManyToOne(inversedBy: 'stocks')]
+    private ?Produit $produit = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmplacement(): ?string
+    {
+        return $this->emplacement;
+    }
+
+    public function setEmplacement(string $emplacement): self
+    {
+        $this->emplacement = $emplacement;
+
+        return $this;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(string $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getNbreProdEnstock(): ?int
+    {
+        return $this->nbreProdEnstock;
+    }
+
+    public function setNbreProdEnstock(int $nbreProdEnstock): self
+    {
+        $this->nbreProdEnstock = $nbreProdEnstock;
+
+        return $this;
+    }
+
+    public function getProduit(): ?Produit
+    {
+        return $this->produit;
+    }
+
+    public function setProduit(?Produit $produit): self
+    {
+        $this->produit = $produit;
+
+        return $this;
+    }
+}
